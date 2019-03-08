991a1d18fe13c3a8510b50f3da689b9e25cf6148
Admin Default <admin@default.com>
2019-03-08 06:58:39 -0600

    Initial Commit

---
+++ src/main/java/g1/LeanFtTest.java
@@ -0,0 +1,58 @@
+package g1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import com.hp.lft.sdk.*;
+import com.hp.lft.verifications.*;
+import com.hp.lft.sdk.web.*;
+
+import unittesting.*;
+
+public class LeanFtTest extends UnitTestClassBase {
+
+    public LeanFtTest() {
+        //Change this constructor to private if you supply your own public constructor
+    }
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        instance = new LeanFtTest();
+        globalSetup(LeanFtTest.class);
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        globalTearDown();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() throws GeneralLeanFtException {
+        Browser browser = BrowserFactory.launch(BrowserType.CHROME);
+
+        browser.navigate("http://advantageonlineshopping.com/#/");
+
+        Link sPEAKERSLink = browser.describe(Link.class, new LinkDescription.Builder()
+                .innerText("SPEAKERS")
+                .tagName("SPAN").build());
+        sPEAKERSLink.click();
+
+        Button buyNowButton = browser.describe(Button.class, new ButtonDescription.Builder()
+                .buttonType("submit")
+                .name("BUY NOW")
+                .tagName("BUTTON").build());
+        buyNowButton.click();
+
+    }
+
+}
